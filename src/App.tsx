a1635d2509dc7dac97b8cc70b343db44bc74612f
Roman Khassazhirov <roman.khassazhirov@gmail.com>
2024-11-28 10:37:06 +0300

    Add SettingCounterBlock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Counter } from './Counter';
+import { SettingCounterBlock } from './SettingsCounterBlock';
 
 function App() {
   
   const [count, setCount] = useState(0)
+  const [startValue, setStartValue] = useState(0)
+  const [maxValue, setMaxValue] = useState(0)
 
   return (
     <div className="App">
       <div className="container-big">
+        <SettingCounterBlock startValue={startValue}
+                             maxValue={maxValue}          
+        />
         <Counter value={count} onChange={setCount} />
       </div>
     </div>
--- src/SettingsCounterBlock.tsx
+++ src/SettingsCounterBlock.tsx
@@ -1,19 +1,26 @@
 import { Button } from "./Button"
 
-type PropsType = {}
+type PropsType = {
+  startValue: number
+  maxValue: number
+}
 
-export const SettingCounterBlock = () => {
+export const SettingCounterBlock = ({startValue, maxValue}: PropsType) => {
 
   return (
     <div className="settings-counter container">
       <div className="settings-counter__block">
         <div className="settings-counter__max-box settings-counter--flex">
           <span>max value:</span> 
-          <input type="number" />
+          <input type="number" 
+                 value={maxValue}
+          />
         </div>
         <div className="settings-counter__start-box settings-counter--flex">
           <span>start value:</span> 
-          <input type="number" />
+          <input type="number" 
+                 value={startValue}
+          />
         </div>
       </div>
       <div className="counter__buttons">
